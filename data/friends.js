809db0058abfd81e77a77bd1533c50895df8dbc3
Jason Vermaak <Jasonvermaak@gmail.com>
2018-12-13 11:35:38 -0500

    images anf folders

---
+++ data/friends.js
@@ -0,0 +1,40 @@
+var employeesArray = [{
+    name: "Chandler",
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPsQh_8A8udqftWO37syjexOn27RyC8rjKGbUBaPZNvJGJmO7A",
+    scores: [
+        1, 1, 1, 5, 1, 2, 3, 2, 2, 5
+    ]
+},
+{
+    name: "Joey",
+    photo:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuSkSzHgblkFmfXD_qEYvf-Yekyq3NhCASNnpoyzPJv1yBFeE ",
+    scores: [
+        5, 1, 1, 4, 5, 4, 3, 4, 4, 4
+    ]
+},
+{
+    name: "Monica",
+    photo:"https://gl-images.condecdn.net/image/E4087aPZGGN/crop/405/f/courteney-cox-22jun17-GettyImages-b.jpg ",
+    scores: [
+        1, 3, 1, 5, 1, 2, 5, 1, 1, 5
+    ]
+},
+{
+    name: "Rachel",
+    photo:"http://cdn.collider.com/wp-content/uploads/2015/02/jennifer-aniston-friends.jpg",
+    scores: [
+        5, 5, 5, 2, 3, 2, 3, 1, 3, 4
+    ]
+},
+{
+    name: "Ross",
+    photo:"https://vignette.wikia.nocookie.net/warner-bros-entertainment/images/d/dd/6FA26EB3-AB8E-41B5-80B3-63555E1E044D.jpeg/revision/latest?cb=20171026033035",
+
+    scores: [
+        5, 5, 5, 1, 5, 1, 3, 1, 5, 2
+    ]
+}
+
+]
+
+module.exports = employeesArray;
